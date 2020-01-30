3dd7b5bf5ee266285eff23ca457f5c1d9fd8c4e0
Nima Bigdely-Shamlo <nima.bigdely-shamlo@teradata.com>
2020-01-29 18:43:27 -0800

    Added basic save functions. Needs testing.

--- varpack/__init__.py
+++ varpack/__init__.py
@@ -7,6 +7,23 @@ import os
 PICKLE_FILENAME = 'all_pickled_vars.p'
 INTERNAL_VARNAME = '__internal__'
 
+
+class NumpyArrayPlaceholder:
+    def __init__(self, np_arr=None, save_folder=None, var_name=None, key_hash=None):
+        if np_arr is not None:
+            filename = os.path.join(save_folder, var_name + str(key_hash) + '.npy')
+            try:
+                # need to allow pickle here since no other way to save mixed numpy and Python objects
+                np.save(filename, np_arr, allow_pickle=True)
+                self.filename = filename
+            except:
+                print('Failed in saving numpy placeholder file:', filename)
+                self.filename = None  # means it was not successful
+
+    def load(self):
+        pass
+
+
 class VarPack:
 
     def __init__(self, max_dict_keys=1000, min_dict_numpy_size=10000):
@@ -30,21 +47,28 @@ class VarPack:
                 except:
                     pickle_vars.append(var_name)
             else:  # cannot readily be saved as a numpy array
+                pickle_vars.append(var_name)
 
                 # see if it is dictionary made up of numpy arrays (and not has too many keys)
-                if type(obj_vars[var_name]) is dict and len(obj_vars[var_name]) < self.INTERNAL_VARNAME['max_dict_keys']:
+                if type(obj_vars[var_name]) is dict and len(obj_vars[var_name]) < self.INTERNAL_VARNAME[
+                    'max_dict_keys']:
                     for k in obj_vars[var_name]:
+                        # if the key is a numpy array and has enough elements that makes it worth saving as a
+                        # separate file.
                         if isinstance(obj_vars[var_name][k], np.ndarray) and \
                                 obj_vars[var_name][k].size >= self.INTERNAL_VARNAME['min_dict_numpy_size']:
-                            pass
-
-                else:
-                    pickle_vars.append(var_name)
+                            numpy_array_placeholder = NumpyArrayPlaceholder(obj_vars[var_name][k],
+                                                                            save_folder=save_folder, var_name=var_name,
+                                                                            key_hash=k.__hash__())
+                            # if saving the value as a numpy array was successful,
+                            # put the placeholder object there instead.
+                            if numpy_array_placeholder.filename is not None:
+                                obj_vars[var_name][k] = numpy_array_placeholder
 
         # save the rest of variables as pickle
         pickle_dict = dict()
         for v in pickle_vars:
-            pickle_dict[v] = self.__getattribute__(self, v)
+            pickle_dict[v] = self.__getattribute__(v)
 
         with open(os.path.join(save_folder, PICKLE_FILENAME), 'wb') as f:
             pickle.dump(pickle_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
